25466fe6e2a5d7b26ef1212c6aa3f53f1b6128f6
Sarah Tupman <sarah.tupman@medicanimal.com>
2019-06-24 14:59:44 +0100

    16. Creating Nested Routes -- Create a matching .vue file alongside a directory to use <nuxt-child /> syntax to share common functionality across all children pages

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,21 +1,3 @@
 <template>
-  <div>
-    <input type="text" v-model="userId">
-    <button @click="onLoadUser">Load User</button>
-  </div>
+  <p>Plese enter an id and click the button</p>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      userId: ''
-    }
-  },
-  methods: {
-    onLoadUser() {
-      this.$router.push('/users/' + this.userId)
-    }
-  }
-}
-</script>
