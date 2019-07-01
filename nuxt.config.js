26bcc00019f242add084f145cf3e42aebc34c82e
Sarah Tupman <sarah.tupman@medicanimal.com>
2019-07-01 17:17:27 +0100

    30. Adding the Header Component -- Adding global navigation with mobile side nav. Emit events are used to show/hide depending on user interaction

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans&display=swap' },
     ]
   },
   /*
