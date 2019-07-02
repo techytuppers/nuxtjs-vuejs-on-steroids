cd4755a3d194a3c2ce1f3d67a5a1d8a26a877a59
Sarah Tupman <sarah.tupman@medicanimal.com>
2019-07-02 16:04:38 +0100

    34. Preparing the Backend -- Creating smaller UI components for editing/creating a post. Admin area. Controlling when a blog link goes to edit or view

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,18 +2,21 @@
   <section class="post-list">
     <PostPreview 
       id="1"
+      :is-admin="isAdmin"
       title="Well hello there!"
       previewText="This is my first post!"
       thumbnail="http://placekitten.com/700/400"
     />
     <PostPreview 
       id="2"
+      :is-admin="isAdmin"
       title="Hello there!"
       previewText="This is my second post!"
       thumbnail="http://placekitten.com/700/400"
     />
     <PostPreview 
       id="3"
+      :is-admin="isAdmin"
       title="Hello there again!"
       previewText="This is my third post!"
       thumbnail="http://placekitten.com/700/400"
@@ -27,6 +30,12 @@ import PostPreview from '@/components/Posts/PostPreview';
 export default {
   components: {
     PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
   }
 }
 </script>
--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,3 +1,39 @@
 <template>
-  
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost"/>
+    </section>
+  </div>
 </template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm';
+
+export default {
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: 'Sarah Tupman',
+        title: 'My awesome post',
+        content: 'Super amazing content goes here.',
+        thumbnailLink: 'http://placekitten/700/500'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
