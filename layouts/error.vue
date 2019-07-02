dc10e94f769f2c5a235b49533dcb6b21ed606d66
Sarah Tupman <sarah.tupman@medicanimal.com>
2019-07-02 18:06:42 +0100

    36. Adding a Separate Layout to the Admin Area -- Separate layout for admin pages, specific this deviating layout within each page. Added custom error page, content has a regular anchor, not a <nuxt-link>, that goes to the homepage to avoid the link not working due to a JS app crash

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="error-page">
+    <h1>Oops, something went wrong!</h1>
+    <!-- Use a normal anchor incase our JS crashed, JS app might be dead -->
+    <p>Back to <a href="/">safety</a>!</p>
+  </div>
+</template>
+
+<style scoped>
+.error-page {
+  text-align: center;
+}
+
+.error-page a {
+  text-decoration: none;
+  color: red;
+}
+
+.error-page a:hover,
+.error-page a:active {
+  color: salmon;
+}
+</style>
